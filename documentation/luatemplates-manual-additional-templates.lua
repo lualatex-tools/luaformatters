1674a70704c03d369cf248467764f493e9b241fb
Urs Liska <git@ursliska.de>
2019-07-11 20:08:15 +0200

    Add function \term
    This is a pretty complex function with a closure keeping track of
    used terms, printing only the first occurrence of a term in colors.

--- documentation/luatemplates-manual-additional-templates.lua
+++ documentation/luatemplates-manual-additional-templates.lua
@@ -1,6 +1,31 @@
 
 local ADDITIONALS = {}
 
+local function term()
+--[[
+    Local function with a closure to keep track of used \term elements.
+    A term is printed in monospace font, but the *first* appearance of a
+    term will additionally be colored!
+    The optional argument may be used to specify the core form of a term.
+--]]
+    local used_terms = {}
+
+    return function (self, term, options)
+        local ref_term = term
+        if options ~= '' then ref_term = options end
+        if used_terms[ref_term] then
+            return self:wrap_macro('texttt', term)
+        else
+            used_terms[ref_term] = true
+            return self:wrap_macro('textcolor', {
+                'OliveGreen',
+                self:wrap_macro('texttt', term)
+            })
+        end
+    end
+end
+
+
 ADDITIONALS.formatters = {
     cary = [[Mary Flagler Cary Music Collection \emph{(Pierpont Morgan Library)}]],
     DV = {
@@ -17,6 +42,14 @@ ADDITIONALS.formatters = {
         comment = 'A LaTeX package, Lua module etc.',
         color = 'olive'
     },
+    term = {
+        -- use a local function with a closure, keeping track of uses.
+        f = term(),
+        comment = 'A specific term or name',
+        -- coloring is done *inside* the function itself
+        color = 'nocolor',
+    }
 }
+
 
 return ADDITIONALS
