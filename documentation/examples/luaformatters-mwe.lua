e70b9d1acf11fcafe02088aeb99d7227fe1f9adf
Urs Liska <git@ursliska.de>
2019-07-26 10:40:36 +0200

    Update MWE (make it really minimal)

--- documentation/examples/luaformatters-mwe.lua
+++ documentation/examples/luaformatters-mwe.lua
@@ -1,21 +1,7 @@
 local MWE = lua_formatters:new_client('mwe')
-
-function MWE.formatters:XXX(text)
-    return text:gsub('.', 'X')
+MWE:add_formatter('red', [[\textcolor{red}{<<<text>>>}]])
+function MWE.formatters:optReverse(text, options)
+    if options.reverse then text = text:reverse() end
+    return text
 end
-
-MWE:add_formatters('arbitrary comment', {
-    cmd = [[\texttt{\textbf{\textbackslash <<<name>>>}}]],
-    package = {
-        f = [[\texttt{<<<name>>>}]],
-        color = 'magenta',
-        comment = "A comment"
-    },
-})
-
-MWE:add_configuration{
-    bold = 'myBold',
-    cmd = { color = 'magenta' }
-}
-
 return MWE
